aa2b37a777d92ebc1372fbcbeb70ca3e19701594
nikawa2161 <nikawa.2161.t@gmail.com>
2022-01-26 23:12:37 +0900

    item.model CategoryClass add

--- base/models/item_models.py
+++ base/models/item_models.py
@@ -9,6 +9,12 @@ def upload_image_to(instance, filename): #アイテムが作成されたとき
     item_id = instance.id
     return os.path.join('static', 'items', item_id, filename)
 
+class Category(models.Model):
+    slug = models.CharField(max_length=32, primary_key=True) #URLに表示するためidを変数に
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
 
 class Item(models.Model): #Djangoが用意している、Modelを持ってきている
     id = models.CharField(default=create_id, primary_key=True, max_length=22, editable=False) #default=create_idにて上の関数を呼び出しランダムな数字をIDに入力する
@@ -21,6 +27,8 @@ class Item(models.Model): #Djangoが用意している、Modelを持ってきて
     created_at = models.DateTimeField(auto_now_add=True) #作成された日
     updated_at = models.DateTimeField(auto_now=True) #アイテムの情報が更新された日
     image = models.ImageField(default="", blank=True,upload_to=upload_image_to)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True) # models.SET_NULL 参照されるアイテムが消されるとアイテムを参照できなくなるため、nullにする
 
     def __str__(self):
         return self.name
