1a2e6fedba3613625f2f494bbf7efedefd553c6a
nikawa2161 <nikawa.2161.t@gmail.com>
2022-01-26 21:38:31 +0900

    gitigbore add

--- config/settings.py
+++ config/settings.py
@@ -11,23 +11,33 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ #追記
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#追記
+env = environ.Env()
+root = environ.path(BASE_DIR / 'secrets')
+
+# 本番環境用
+# env.read_env(root('.env.prod'))
+
+# 開発環境用
+env.read_env(root('.env.dev'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ggvx6&r5s0hzo1z7_#cu#6-hhj6-adk#)cal23fxs*i=z-vq*h'
+SECRET_KEY = env.str('SECRET_KEY')
 
 #シークレットキーはギットハブに絶対にあげない
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
