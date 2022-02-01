1138642c4a1f0a5f629ff4de64a50ba8cd01adbc
nikawa2161 <nikawa.2161.t@gmail.com>
2022-02-01 16:42:45 +0900

    Stripe settlement create

--- config/urls.py
+++ config/urls.py
@@ -1,19 +1,3 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from base.views.cart_views import AddCartView, CartListView
 from django.contrib import admin
 from django.urls import path
 from base import views
@@ -21,12 +5,15 @@ from base import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    #cart
+    # Pay
+    path('pay/checkout/', views.PayWithStripe.as_view()),
+    path('pay/success/', views.PaySuccessView.as_view()),
+    path('pay/cancel/', views.PayCancelView.as_view()),
+
+    # Cart
     path('cart/remove/<str:pk>/', views.remove_from_cart),
     path('cart/add/', views.AddCartView.as_view()),
-
-
-    path('cart/', views.CartListView.as_view()), #カートページ
+    path('cart/', views.CartListView.as_view()),  # カートページ
 
     # Items
     path('items/<str:pk>/', views.ItemDetailView.as_view()),
